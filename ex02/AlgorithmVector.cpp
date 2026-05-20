585002964b3ad019ea302c7c186e6697abed7263
Paula Drettas <pauladrettas@gmail.com>
2026-05-20 18:36:07 +0200

    FIX(ex02): adjust insertPendElementsIntoMainChain ft to always get sorted sequence [still in progress]

--- ex02/AlgorithmVector.cpp
+++ ex02/AlgorithmVector.cpp
@@ -6,7 +6,7 @@
 /*   By: pauladrettas <pauladrettas@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/17 20:18:31 by pdrettas          #+#    #+#             */
-/*   Updated: 2026/05/20 03:19:23 by pauladretta      ###   ########.fr       */
+/*   Updated: 2026/05/20 18:34:55 by pauladretta      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,8 +135,8 @@ void PmergeMe::generateJacobsthalInsertionOrder(std::vector<unsigned int> &inser
         for (unsigned int j = currentJtNum; j > previousJtNum; --j) // start at current Jt num, go backwards, until previous jacobsthal num -> for (int j = 5; j > 3; --j) add 4 (so 1 3 2 5 4)
             insertionOrder.push_back(j);
 
-        if (currentJtNum == 1)
-            insertionOrder.push_back(1);
+        // if (currentJtNum == 1)
+        //     insertionOrder.push_back(1);
 
         previousJtNum = currentJtNum;
     }
@@ -153,13 +153,27 @@ void PmergeMe::generateJacobsthalInsertionOrder(std::vector<unsigned int> &inser
 void PmergeMe::insertPendElementsIntoMainChain(std::vector<unsigned int> &insertionOrder, std::vector<std::pair<unsigned int, unsigned int>> &pairs, std::vector<unsigned int> &smallerElementSequence, std::vector<unsigned int> &largerElementSequence)
 {
     // insert first pend element directly according to algorithm
-    if (!smallerElementSequence.empty()) // not needed in loop bc doesnt even enter loop if size is 0/empty
-        largerElementSequence.insert(largerElementSequence.begin(), smallerElementSequence[0]);
+    // if (!smallerElementSequence.empty())
+    // {
+    //  // not needed in loop bc doesnt even enter loop if size is 0/empty
+    //     largerElementSequence.insert(largerElementSequence.begin(), smallerElementSequence[insertionOrder[0] - 1]);
+    // std::cout << "paula " << *largerElementSequence.begin() << std::endl;
+    // std::cout << "pppp " << smallerElementSequence[insertionOrder[0] - 1] << std::endl;
+    // }
 
     for (int i = 0; i < static_cast<int>(insertionOrder.size()); i++) // loop goes thru jacobsthal insertion order (1, 3, 2, 5, 4, ...)
     {
+        // TODO: apparently here and above is a bug somewhere here. ./PmergeMe 5 2 6 9 4 3 7 4 2 3 1 2 3 4 8 9 4 3 2 4 2 5 8 2 3 5 6 does not get sorted.
         if (insertionOrder[i] == 1)
-            continue;
+        {
+            if (!smallerElementSequence.empty())
+            {
+                largerElementSequence.insert(largerElementSequence.begin(), smallerElementSequence[0]);
+                std::cout << "*begin oben " << *largerElementSequence.begin() << std::endl;
+                std::cout << "element to add oben " << smallerElementSequence[0] << std::endl;
+            }
+            continue;       
+        }
     
         int index = insertionOrder[i] - 1; // index needs to be -1 bc (vector) indexes start w 0 // (-1) bc ex. looking for third element in smallerElementSequence which has index 2
         unsigned int elementToInsert = smallerElementSequence[index]; // this needs to be inserted in he largerElemenSequence/mainChain (with .insert) 
@@ -169,16 +183,20 @@ void PmergeMe::insertPendElementsIntoMainChain(std::vector<unsigned int> &insert
         // NOW, we need to find its larger partner from initial pair (which also appears in the largerElementsequence)
         unsigned int largerPairPartner = pairs[index].second; // the index in the smallerElementSequence is the same index in vec pairs for the same number.
         std::cout << PINK << "pairLargerPartner: " << largerPairPartner << RESET << std::endl;
-
+        
         // look for position of largerPairPartner in largerElementSequence/main chain
         auto positionLargerPairPartner = std::find(largerElementSequence.begin(), largerElementSequence.end(), largerPairPartner); 
 
         // find where to insert smaller element based on its number in the range of beginning to larger pair element in main chain
-        // TODO: apparently there is a bug somewhere here. ./PmergeMe 5 2 6 9 4 3 7 4 2 3 1 2 3 4 8 9 4 3 2 4 2 5 8 2 3 5 6 does not get sorted.
         auto positionElementToInsert = std::lower_bound(largerElementSequence.begin(), positionLargerPairPartner, elementToInsert);
 
         // insert smaller element
         largerElementSequence.insert(positionElementToInsert, elementToInsert);
+        
+        std::cout << "* begin unten " << *largerElementSequence.begin() << std::endl;
+        std::cout << "element to instert unetn " << elementToInsert << std::endl;
+        std::cout << "*position unten " << *positionElementToInsert << std::endl;
+        // exit(0);
     }
 }
 
